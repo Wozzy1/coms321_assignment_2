a7bcfe3ddd0662195e065f519f2ec5f44698a9e0
Wozzy1 <134657893+Wozzy1@users.noreply.github.com>
2024-11-13 17:03:41 -0600

    created skeleton code

--- main.py
+++ main.py
@@ -2,12 +2,11 @@
 # diepw50@iastate.edu
 # COMS 321 Programming Assignment 2
 # 11/13/12024
+from BinaryToASM import BinaryToASM
 
-"""
-* create map of opcodes to ASM words
-* write separate functions to separate each of the fields depending on opcode
-* 
+def main():
+    Converter:BinaryToASM = BinaryToASM()
+    print("Output: " + Converter.get_instruction_from_opcode(Converter.INSTRUCTIONS, 0b1001000100))
 
-"""
-
-
+if __name__ == '__main__':
+    main()
---
+++ test.py
@@ -0,0 +1,5 @@
+from BinaryToASM import BinaryToASM
+
+b = BinaryToASM()
+
+b.determine_instruction_type(0b1001000100)
