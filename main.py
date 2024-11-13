933df5322bc58ab4a9567e37f2937248f18fd720
Wozzy1 <134657893+Wozzy1@users.noreply.github.com>
2024-11-13 00:49:04 -0600

    initial commit

---
+++ main.py
@@ -0,0 +1,13 @@
+# Wilson Diep
+# diepw50@iastate.edu
+# COMS 321 Programming Assignment 2
+# 11/13/12024
+
+"""
+* create map of opcodes to ASM words
+* write separate functions to separate each of the fields depending on opcode
+* 
+
+"""
+
+
