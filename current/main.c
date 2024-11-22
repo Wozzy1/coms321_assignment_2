37b813b7747ef772212d5e5beaa5d0acb28ce6e0
Wozzy1 <diepw50@gmail.com>
2024-11-22 16:43:50 -0600

    implemented I and D type instructions

--- current/main.c
+++ current/main.c
@@ -2,6 +2,10 @@
 #include<string.h>
 #define NUM_INSTRUCTIONS 25
 
+/**
+ * Custom struct type for a given instruction that 
+ * has a mnemonic, an instruction type, and opcode.
+ */
 typedef struct instruction {
     char mnemonic[6];   // 6 bytes
     char type[3];       // 3 bytes
@@ -55,12 +59,34 @@ void replaceSpecialRegister(char* reg, int r);
  * and return the index of which instruction matches.
  */
 int findInstruction(int opcode) {
-    for (int i = 0; i < NUM_INSTRUCTIONS; i++) {
+    // printf("Attempting to find R type\n");
+    for (int i = 0; i <= NUM_INSTRUCTIONS; i++) {
         if (opcode == instructions[i].opcode) {
             printf("opcode received: %#x\n", opcode);
             return i;
         }
     }
+    // printf("Attempting to find I type\n");
+    for (int i = 0; i <= NUM_INSTRUCTIONS; i++) {
+        if (((opcode >> 1) & 0x7FF) == instructions[i].opcode) {
+            printf("opcode received: %#x\n", (opcode >> 1) & 0x7FF);
+            return i;
+        }
+    }
+
+    
+    // for (int i = 0; i <= NUM_INSTRUCTIONS; i++) {
+    //     if (opcode == instructions[i].opcode) {
+    //         printf("opcode received: %#x\n", opcode);
+    //         return i;
+    //     }
+    // }
+    // for (int i = 0; i <= NUM_INSTRUCTIONS; i++) {
+    //     if (opcode == instructions[i].opcode) {
+    //         printf("opcode received: %#x\n", opcode);
+    //         return i;
+    //     }
+    // }
 
     return -1;
 }
@@ -83,16 +109,31 @@ void createLine(char* line, instruction* inst, int binary) {
         int shamt = (binary >> 10) & 0x3F;
         int rm = (binary >> 16) & 0x1F;
         int opcode = (binary >> 21) & 0x7FF;
-        printf("%X x%d %X %X %X\n", opcode, rm, shamt, rn, rd);
+        // printf("%X x%d %X %X %X\n", opcode, rm, shamt, rn, rd);
 
         // need to check if the register number is special ie, SP, FP, LR, XZR (28:31)
 
-        // if LSL or LSR, then shamt matters
-        /*
-        LSL x1, x1, #3
-        */
+        // if PRNT, PRNL, DUMP, HALT then print only the instruction name
+        if (opcode == 0b11111111101 || opcode == 0b11111111100 || opcode == 0b11111111110 || opcode == 0b11111111111) {
+            sprintf(line, "%-5s ", inst->mnemonic);
+            return;
+        }
 
         sprintf(line, "%-5s ", inst->mnemonic);
+        if (opcode == 0x6B0) { // BR
+            if (28 <= rn && rn <= 31) {
+                char rn_c[4];
+                replaceSpecialRegister(rn_c, rn);
+                // sprintf(line, "%s ", rn_c);
+                strcat(line, rn_c);
+            } else {
+                char temp[5];
+                sprintf(temp, "x%d ", rn);
+                strcat(line, temp);
+            }
+            return;
+        }
+
         // check if rd is special or not
         if (28 <= rd && rd <= 31) {
             char rd_c[4];
@@ -138,8 +179,79 @@ void createLine(char* line, instruction* inst, int binary) {
             }
         }
     } else if (!strcmp(type, "I")) {
+        int rd = binary & 0x1F;
+        int rn = (binary >> 5) & 0x1F;
+        int immediate = (binary >> 10) & 0xFFF;
+        int opcode = (binary >> 22) & 0x3FF;
+        printf("%X x%d x%d %d\n", opcode, rd, rn, immediate);
+
+        sprintf(line, "%-6s", inst->mnemonic);
+        if (28 <= rd && rd <= 31) {
+            char rd_c[4];
+            replaceSpecialRegister(rd_c, rd);
+            // sprintf(line, "%s ", rd_c);
+            strcat(line, rd_c);
+            strcat(line, ", ");
+        } else {
+            char temp[5];
+            sprintf(temp, "x%d, ", rd);
+            strcat(line, temp);
+        }
+        // check if rn is special or not
+        if (28 <= rn && rn <= 31) {
+            char rn_c[4];
+            replaceSpecialRegister(rn_c, rn);
+            // sprintf(line, "%s ", rn_c);
+            strcat(line, rn_c);
+            strcat(line, ", ");
+        } else {
+            char temp[5];
+            sprintf(temp, "x%d, ", rn);
+            strcat(line, temp);
+        }
+
+        char immediate_c[7];
+        sprintf(immediate_c, "#%d", immediate);
+        strcat(line, immediate_c);
 
     } else if (!strcmp(type, "D")) {
+        int rt = binary & 0x1F;
+        int rn = (binary >> 5) & 0x1F;
+        // int op = (binary >> 10) & 0x3; // unused two bits
+        int dt_address = (binary >> 12) & 0x1FF;
+        int opcode = (binary >> 21) & 0x7FF;
+
+        printf("%X x%d [x%d, #%d]\n", opcode, rt, rn, dt_address);
+
+        sprintf(line, "%-5s", inst->mnemonic);
+
+        if (28 <= rt && rt <= 31) {
+            char rt_c[4];
+            replaceSpecialRegister(rt_c, rt);
+            strcat(line, rt_c);
+            strcat(line, ", [");
+        } else {
+            char temp[5];
+            sprintf(temp, "x%d, [", rt);
+            strcat(line, temp);
+        }
+        // check if rn is special or not
+        if (28 <= rn && rn <= 31) {
+            char rn_c[6];
+            replaceSpecialRegister(rn_c, rn);
+            // sprintf(line, "%s ", rn_c);
+            strcat(line, rn_c);
+            strcat(line, ", ");
+        } else {
+            char temp[6];
+            sprintf(temp, "x%d, ", rn);
+            strcat(line, temp);
+        }
+        char offset[6];
+
+        sprintf(offset, "#%d]", dt_address);
+        strcat(line, offset);
+
 
     } else if (!strcmp(type, "B")) {
 
@@ -152,6 +264,8 @@ void createLine(char* line, instruction* inst, int binary) {
 /**
  * Core method that takes a 32 bit instruction, disassembles it,
  * and prints it to the console (for now).
+ * TODO update method to take a pointer of array of all dissassembled
+ * instructions.
  */
 void decode_instruction(int binary) {
     int opcode = (binary >> 21) & 0x7FF;
@@ -161,7 +275,7 @@ void decode_instruction(int binary) {
     char line[25];
     createLine(line, &instructions[res], binary);
 
-    printf("%s\n", line);
+    printf("  %s\n", line);
     
 }
 
@@ -227,12 +341,31 @@ void replaceSpecialRegister(char* reg, int r) {
 
 }
 
+/**
+ * Count number of lines after disassembling 
+ * create array of boolean assign 1 if "label_index:\n" needs to be displayed
+ */
+
 int main() {
     // SUB X12, X10, XZR = 11001011000111110000000101001100
 
     // printInstruction(&instructions[0]);
     // decode_instruction(0b11001011000111110000000101001100);
-    decode_instruction(0xD360F8E3);
+    
+//     int nums[] = { 
+// 0x91002BE1,
+// 0x92002822,
+// 0xB20003E3,
+// 0xD2000441,
+// 0xD1002022,
+// 0xF1003C24
+//     };
+
+    // for (int i = 0; i < 6; i++) {
+    //     decode_instruction(nums[i]);
+    // }
+    decode_instruction(0xF8400061);
+    decode_instruction(0xF8008384);
     // printf("Count %u\n", countBits(0b10001011000111110000001111100000));
     // printf("%lu", sizeof(int));
 
